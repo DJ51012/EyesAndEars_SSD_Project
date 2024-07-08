289466a096f6d05d7ea16e59ff8b468701c41a00
kimjinhong <jh0703.kim@gmail.com>
2024-07-08 16:40:40 +0900

    [feature] update basic read cmd TC
    - update BasicReadCmdSuccess
    - add class FileIoInterface
    - add class MockFileIO
    
    Signed-off-by: kimjinhong <jh0703.kim@gmail.com>

--- TestShell/testshell.cpp
+++ TestShell/testshell.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include "../SSD_Project/SsdDriver.h"
+#include "../TestShell/FileIOInterface.h"
 
 using namespace std;
 
@@ -7,6 +9,12 @@ public:
 	TestShell(string cmd) : cmd(cmd) {
 
 	}
+	void setDriver(SsdDriver* driver)  {
+		this->driver = driver;
+	}
+	void setFileIo(FileIoInterface* fio) {
+		this->fio = fio;
+	}
 
 	bool run_cmd() {
 		return false;
@@ -14,4 +22,7 @@ public:
 
 private:
 	string cmd;
+	SsdDriver* driver;
+	FileIoInterface* fio;
+
 };
