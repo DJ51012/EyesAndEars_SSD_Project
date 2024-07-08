88c1053721a255c541753e4147ac5964bb028f5b
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-08 15:00:47 +0900

    [feature] assert for wrong arguments

--- TestShell/testshell.cpp
+++ TestShell/testshell.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 class TestShell {
 public:
-	TestShell(string cmd) : cmd(cmd) {
+	TestShell(string cmd, vector<string> args) : cmd(cmd), args(args) {
 
 	}
 
 	bool run_cmd() {
 		AssertWrongCmd();
+		AssertWrongArguments();
+
 		return false;
 	}
-
 
 private:
 	void AssertWrongCmd()
@@ -29,5 +30,15 @@ private:
 		throw invalid_argument("Undefined test command!");
 	}
 
+	void AssertWrongArguments()
+	{
+		if (cmd == "write" && args.size() >= 2 && stoi(args[0]) < 100 && args[1].length() == 10 && args[1].substr(0, 2) == "0x") return;
+		if (cmd == "read" && args.size() >= 1 && stoi(args[0]) < 100) return;
+		if (cmd == "fullwrite" && args.size() >= 1 && args[0].length() == 10 && args[0].substr(0, 2) == "0x") return;
+			
+		throw invalid_argument("wrong argument for fullwrite!");
+	}
+
 	string cmd;
+	vector<string> args;
 };
