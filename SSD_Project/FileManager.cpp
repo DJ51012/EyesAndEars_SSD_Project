07de4fc110e61ae52c177052bf4d7d6542afd5c8
Dongjin <triumph0510@gmail.com>
2024-07-09 11:44:04 +0900

    ﻿Add File Input/Output in File Manager

--- SSD_Project/FileManager.cpp
+++ SSD_Project/FileManager.cpp
@@ -1,14 +1,77 @@
 #include "FileManager.h"
 
 FileManager::FileManager() {
-	for (int i = 0; i < MAX_LBA; i++)
-		nand[i] = CLEAN_DATA;
+    createFile();
 }
 
-string FileManager::readNand(int lba) {
-	return nand[lba];
+void FileManager::writeNand(unsigned int line, string value) {
+    vector<string> readLines;
+    string readLine;
+
+    fstream nandFile = getNandFile();
+    if (nandFile.is_open()) {
+        while (getline(nandFile, readLine)) {
+            readLines.push_back(readLine);
+        }
+    }
+    validLineNumCheck(line);
+
+    readLines[line] = value;
+    nandFile.close();
+
+    ofstream nandFileWrite(NAND_FILE_NAME);
+
+    for (const auto& ln : readLines) {
+        nandFileWrite << ln << endl;
+    }
+    nandFileWrite.close();
 }
 
-void FileManager::writeNand(int lba, string data) {
-	nand[lba] = data;
+void FileManager::readNand(unsigned int line) {
+    fstream nandFile = getNandFile();
+    ofstream resultFile = getResultFile();
+
+    string readLine;
+    int lineNumber = 0;
+    string value;
+
+    while (getline(nandFile, readLine)) {
+        if (lineNumber == line) {
+            resultFile << readLine << endl;
+            break;
+        }
+        lineNumber++;
+    }
+
+    nandFile.close();
+    resultFile.close();
 }
+
+void FileManager::createFile() {
+    ofstream nandfile(NAND_FILE_NAME);
+    for (int idx = 0; idx <= MAX_LINE_NUM; idx++) {
+        nandfile << DEFAULT_WRITE_VALUE << endl;
+    }
+    nandfile.close();
+}
+
+fstream FileManager::getNandFile() {
+    ifstream nandfileCheck(NAND_FILE_NAME);
+    if (!nandfileCheck.is_open()) {
+        createFile();
+    }
+    fstream nandfile(NAND_FILE_NAME);
+    return nandfile;
+}
+
+ofstream FileManager::getResultFile() {
+    ofstream resultFile(RESULT_FILE_NAME);
+    return resultFile;
+}
+
+void FileManager::validLineNumCheck(int line) {
+    if (line < MIN_LINE_NUM || line > MAX_LINE_NUM) {
+        cerr << "Invalid line number" << endl;
+    }
+}
+
--- SSD_Project/FileManager.h
+++ SSD_Project/FileManager.h
@@ -1,20 +1,33 @@
 #pragma once
 
+#include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class FileManager {
 public:
-	const string CLEAN_DATA = "0x00000000";
-
-	FileManager();
-	string readNand(int lba);
-	void writeNand(int lba, string data);
+    static FileManager& getInstance() {
+        static FileManager instance;
+        return instance;
+    }
+    void writeNand(unsigned int line, string value);
+    void readNand(unsigned int line);
 
 private:
-	const string FILE_NAME = "nand.txt";
-	const int MAX_LBA = 100;
-	string nand[100];
+    FileManager();
+    FileManager& operator=(const FileManager& otherInstance) = delete;
+    FileManager(const FileManager& otherInstance) = delete;
+    void createFile();
+    fstream getNandFile();
+    ofstream getResultFile();
+    void validLineNumCheck(int line);
+
+    const int MAX_LINE_NUM = 99;
+    const int MIN_LINE_NUM = 0;
+    const string NAND_FILE_NAME = "nand.txt";
+    const string RESULT_FILE_NAME = "result.txt";
+    const string DEFAULT_WRITE_VALUE = "0x00000000";
 };
