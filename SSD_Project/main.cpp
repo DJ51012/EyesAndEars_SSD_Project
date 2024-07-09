2acd29a504d1c4e44f7f3da64e298acba3e87a10
Myeong Woo Kim <mkim0628@gmail.com>
2024-07-09 11:59:36 +0900

    [refactor] Minor Bugs fix and refactoring

--- SSD_Project/main.cpp
+++ SSD_Project/main.cpp
@@ -1,3 +1,15 @@
-int main() {
+#include "CommandManager.h"
+#include "Ssd.cpp"
 
-}
+// Test code
+//int main(int argc, char *argv[]) {
+//    Ssd ssd;
+//    CommandManager cm;
+//
+//    int _argc = 4;
+//    char* _argv[] = { (char *)"ssd", (char*)"W", (char*)"40", (char*)"0xABCD1234" };
+//    if (!cm.IsValidCommand(_argc, _argv))
+//        return false;
+//
+//    cm.executeSSDCommand(&ssd);
+//}
