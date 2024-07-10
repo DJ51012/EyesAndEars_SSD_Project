8278be505d0fde9eeadf0a396a5fa3e8f5170139
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-10 15:35:37 +0900

    [fix] wrong file descriptor handling

--- TestShell/testcmd.h
+++ TestShell/testcmd.h
@@ -31,7 +31,9 @@ public:
 		else {
 			char buf[MAX_BUF_SIZE];
 			memset(buf, 0, MAX_BUF_SIZE);
-			fio->Read((int)fd, buf, ONE_LINE_SIZE);
+			fio->Read(_fileno(fd), buf, ONE_LINE_SIZE);
+			buf[ONE_LINE_SIZE] = 0;
+			fclose(fd);
 			std::cout << buf;
 		}
 	}
@@ -80,10 +82,11 @@ public:
 			else {
 				char buf[MAX_BUF_SIZE];
 				memset(buf, 0, MAX_BUF_SIZE);
-				int result = fio->Read((int)fd, buf, ONE_LINE_SIZE);
+				int result = fio->Read(_fileno(fd), buf, ONE_LINE_SIZE);
+				buf[ONE_LINE_SIZE] = 0;
+				fclose(fd);
 				if (result == 0) return;
 				std::cout << buf;
-				if (fd) fclose(fd);
 			}
 		}
 
