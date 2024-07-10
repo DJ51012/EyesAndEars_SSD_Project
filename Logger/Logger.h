1fe75acd3f55ad6b025bab1e899165bb0eded0bc
Dongjin <triumph0510@gmail.com>
2024-07-10 19:22:23 +0900

    store log file

--- Logger/Logger.h
+++ Logger/Logger.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
 
@@ -21,6 +23,26 @@ private:
 	string getOrganizedFunctionName(const char functionName[]);
 	time_t getNow();
 	string toStringWithZeroPadding(int number);
-	string getDateTime();
+	string getDateTimeForLog();
+	string getFileNameForStore();
+	string getYear(tm* tmTime);
+	string getMonth(tm* tmTime);
+	string getDay(tm* tmTime);
+	string getHour(tm* tmTime);
+	string getMinute(tm* tmTime);
+	string getSecond(tm* tmTime);
+
+	fstream getLogFile();
+	void createLogFile();
+	int getLogFileSize();
+	void changePrevFile();
+	string findPrevLogFile();
+
+	const int MAX_LOG_FILE_SIZE = 10 * 1024;
+	const string LATEST_LOG_FILE_NAME = "latest.log";
+	const string LOG_EXTENSION = ".log";
+	const string ZIP_EXTENSION = ".zip";
+
+	//string previousLogFile = "";
 };
 
